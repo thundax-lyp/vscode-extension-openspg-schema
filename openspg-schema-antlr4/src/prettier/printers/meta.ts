33374197b500f600cdf2c761802422a3341d808a
thundax-lyp <731378491@qq.com>
2025-11-24 10:16:08 +0800

    init

--- openspg-schema-antlr4/src/prettier/printers/meta.ts
+++ openspg-schema-antlr4/src/prettier/printers/meta.ts
@@ -4,9 +4,12 @@ import {BasePrinter, PrintFunc} from './base';
 export class PrinterMeta extends BasePrinter implements Record<`print${ast.MetaNodeType}`, PrintFunc<any>> {
 
     printSourceUnit: PrintFunc<ast.SourceUnit> = ({path, print}) => {
-        return this.builders.join(
-            [this.builders.line, this.builders.line], path.map(print, "nodes")
-        );
-    };
+        return [
+            this.builders.join(
+                [this.builders.hardline, this.builders.line], path.map(print, "nodes")
+            ),
+            this.builders.hardline
+        ]
+    }
 
 }
--- openspg-schema-antlr4/test/prettier.test.ts
+++ openspg-schema-antlr4/test/prettier.test.ts
@@ -1,6 +1,7 @@
 import {expect, test} from 'vitest';
 import * as prettier from 'prettier/standalone';
 import {parserName, plugin} from '../src';
+import {load} from "./utils.test";
 
 const format = async (code: string) =>
     prettier.format(code, {
@@ -11,85 +12,13 @@ const format = async (code: string) =>
     });
 
 test('comment', async () => {
-    const sourceCode = `
-/*
- *top comment
- */
-#  top line comment
-namespace Sample /*****line block comment ******/
-
-  /* 2nd block comment */
-
-  # 2nd line comment
-
-Person(人物): EntityType  # 2nd tail comment
-
-    /*  3rd block comment  */
-
-    ##  3rd line comment
-
-    desc: description     #  3rd tail comment
-
-/*** 4th block comment ***/
-
-#  4th line comment
-
-    desc2: description2   #   4th tail comment
-`
-
-    const formattedCode = `/**
- * top comment
- */
-# top line comment
-namespace Sample /* line block comment */
-
-/* 2nd block comment */
-
-# 2nd line comment
-
-Person(人物): EntityType # 2nd tail comment
-    /* 3rd block comment */
-
-    # 3rd line comment
-
-    desc: description # 3rd tail comment
-    /* 4th block comment */
-
-    # 4th line comment
-
-    desc2: description2 # 4th tail comment`
-
-    expect(await format(sourceCode)).toBe(formattedCode)
+    const testFileName = `prettier.comment.schema`
+    const expectedFileName = `${testFileName}.expected`
+    expect(await format(load(testFileName))).toBe(load(expectedFileName))
 });
 
 test('no comment', async () => {
-    const sourceCode = `
-
-namespace DocumentSymbol
-
-Person(人物):   EntityType
-    desc: a great name
-    properties:
-        age(年龄):   Text
-        birth(生日):   Text
-
-Works(作品):    EntityType
-    desc: a great book
-    properties:
-        author(作者):  Person
-`
-    const formattedCode = `namespace DocumentSymbol
-
-Person(人物): EntityType
-    desc: a great name
-    properties:
-        age(年龄): Text
-        birth(生日): Text
-
-Works(作品): EntityType
-    desc: a great book
-    properties:
-        author(作者): Person`
-
-    expect(await format(sourceCode)).toBe(formattedCode)
+    const testFileName = `prettier.no-comment.schema`
+    const expectedFileName = `${testFileName}.expected`
+    expect(await format(load(testFileName))).toBe(load(expectedFileName))
 })
