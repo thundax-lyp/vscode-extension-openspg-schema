6799d46242b36be3aab8a4d5ba74936f7e86cc3e
thundax-lyp <731378491@qq.com>
2025-11-26 10:03:59 +0800

    init

---
+++ openspg-schema-language-server/server/src/core/initialize/on-initialize.ts
@@ -0,0 +1,45 @@
+import {TextDocumentSyncKind} from 'vscode-languageserver';
+import {Context, OnInitialize} from '../context';
+
+
+export const onInitialize = ({connection}: Context): OnInitialize => async ({initializationOptions}) => {
+    connection.console.log(`initializing with options: ${initializationOptions!!}`,);
+
+    return {
+        serverInfo: {
+            name: 'OpenSPG Schema Language Server',
+        },
+        capabilities: {
+            textDocumentSync: {
+                save: true,
+                openClose: true,
+                change: TextDocumentSyncKind.Full,
+                willSave: false,
+                willSaveWaitUntil: false,
+            },
+            // Tell the client that this server supports code completion.
+            // completionProvider: {
+            //   triggerCharacters: ['.', '"', `'`, '*', ' '],
+            // },
+            signatureHelpProvider: {
+                triggerCharacters: ['(', ','],
+            },
+            documentSymbolProvider: true,
+            typeDefinitionProvider: true,
+            definitionProvider: true,
+            referencesProvider: true,
+            hoverProvider: true,
+            // documentLinkProvider: {
+            //     resolveProvider: false
+            // },
+            // codeLensProvider: {
+            //   resolveProvider: false,
+            //   workDoneProgress: false,
+            // },
+            // implementationProvider: true,
+            // renameProvider: true,
+            // codeActionProvider: true,
+            documentFormattingProvider: true,
+        },
+    } as ReturnType<OnInitialize>
+}
