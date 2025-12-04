bcbb8279ad34de208c97d529f2e7159e18826c8e
thundax-lyp <731378491@qq.com>
2025-12-04 15:46:52 +0800

    init

---
+++ vscode-openspg-schema/src/server/context/types.ts
@@ -0,0 +1,23 @@
+import type {Connection} from 'vscode-languageserver';
+import type {Context} from './context';
+
+export type OnDefinition = (ctx: Context) => Parameters<Connection['onDefinition']>[0];
+export type OnTypeDefinition = (ctx: Context) => Parameters<Connection['onTypeDefinition']>[0];
+export type OnReferences = (ctx: Context) => Parameters<Connection['onReferences']>[0];
+export type OnDocumentLinks = (ctx: Context) => Parameters<Connection['onDocumentLinks']>[0];
+export type OnDocumentLinkResolve = (ctx: Context) => Parameters<Connection['onDocumentLinkResolve']>[0];
+export type OnHover = (ctx: Context) => Parameters<Connection['onHover']>[0];
+
+export type OnDocumentHighlight = (ctx: Context) => Parameters<Connection['onDocumentHighlight']>[0];
+
+export type OnSignatureHelp = (ctx: Context) => Parameters<Connection['onSignatureHelp']>[0];
+export type OnImplementation = (ctx: Context) => Parameters<Connection['onImplementation']>[0];
+export type OnDocumentSymbol = (ctx: Context) => Parameters<Connection['onDocumentSymbol']>[0];
+
+export type OnDocumentFormatting = (ctx: Context) => Parameters<Connection['onDocumentFormatting']>[0];
+export type OnDocumentOnTypeFormatting = (ctx: Context) => Parameters<Connection['onDocumentOnTypeFormatting']>[0];
+export type OnDocumentRangeFormatting = (ctx: Context) => Parameters<Connection['onDocumentRangeFormatting']>[0];
+
+export type OnInitialize = Parameters<Connection['onInitialize']>[0];
+export type OnInitialized = Parameters<Connection['onInitialized']>[0];
+export type OnExit = Parameters<Connection['onExit']>[0];
---
+++ vscode-openspg-schema/src/server/definition/on-definition.ts
@@ -0,0 +1,42 @@
+import {Location} from 'vscode-languageserver';
+import {StructureName, TraversePath} from "openspg-schema-antlr4";
+import {OnDefinition} from '../context';
+import {SchemaTextDocument} from "../common/text-document";
+
+
+export const onDefinition: OnDefinition = (ctx) => async (params) => {
+    const {textDocument, position} = params;
+    const document = ctx.documents.get(textDocument.uri);
+    if (!document || !document.ast) {
+        return null;
+    }
+
+    const createSelector = document.createPositionSelector(position);
+    const selectedPath = document.getPathAt<StructureName>(
+        createSelector('StructureName')
+    )
+    if (!selectedPath) {
+        return null;
+    }
+
+    switch (selectedPath.node.type) {
+        case 'StructureName':
+            return handleStructureName(document, selectedPath)
+        default:
+            return null
+    }
+};
+
+const handleStructureName = (document: SchemaTextDocument, selectedPath: TraversePath<StructureName>) => {
+    const selectedParts = selectedPath.path.split('.');
+    if (!selectedParts.includes('BasicStructureTypeExpression')) {
+        return null;
+    }
+
+    return document.ast?.nodes
+        .filter(node => node.type === 'EntityDeclaration')
+        .filter(node => node.declaration.name.variable.realName.text === selectedPath.node.realName.text)
+        .map(node => {
+            return Location.create(document.uri, document.getNodeRange(node.declaration.name))
+        })
+}
