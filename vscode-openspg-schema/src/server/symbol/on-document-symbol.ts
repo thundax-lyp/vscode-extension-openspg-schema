cd848e63f226cede2661d372b4e9307d35c32584
thundax-lyp <731378491@qq.com>
2025-12-10 10:08:33 +0800

    init

--- vscode-openspg-schema/src/server/symbol/on-document-symbol.ts
+++ vscode-openspg-schema/src/server/symbol/on-document-symbol.ts
@@ -1,6 +1,8 @@
 import {DocumentSymbol, SymbolKind} from 'vscode-languageserver';
 import {OnDocumentSymbol} from '../context';
 import {NamespaceDeclaration, EntityDeclaration} from '../common/parser';
+import {BasicPropertyDeclaration, EntityMetaDeclaration, PropertyDeclaration} from "openspg-schema-antlr4";
+import {BasicStructureDeclaration} from "openspg-schema-antlr4/dist/ast/expression";
 
 export const onDocumentSymbol: OnDocumentSymbol = (ctx) => async ({textDocument}) => {
     const document = ctx.documents.get(textDocument.uri);
@@ -8,25 +10,45 @@ export const onDocumentSymbol: OnDocumentSymbol = (ctx) => async ({textDocument}
         return null;
     }
 
-    const handleNamespaceDeclaration = (ast: NamespaceDeclaration): DocumentSymbol | null => {
-        const range = document.getNodeRange(ast);
-        return {
-            name: ast.variable.text,
-            kind: SymbolKind.Namespace,
-            range,
-            selectionRange: range,
-        };
-    };
+    const renderBasicStructureDeclaration = (declaration: BasicStructureDeclaration) => {
+        return [...declaration.name.variable.semanticNames, declaration.name.variable.realName]
+            .map(x => x.text)
+            .join('#')
+    }
 
-    const handleEntityDeclaration = (ast: EntityDeclaration): DocumentSymbol | null => {
-        const range = document.getNodeRange(ast);
-        return {
-            name: [...ast.declaration.name.variable.semanticNames, ast.declaration.name.variable.realName].map(x => x.text).join('#'),
-            kind: SymbolKind.Class,
-            range,
-            selectionRange: range,
-        };
-    };
+    const renderBasicPropertyDeclaration = (declaration: BasicPropertyDeclaration) => {
+        return declaration.name.variable.text
+    }
+
+    const handleNamespaceDeclaration = (ast: NamespaceDeclaration): DocumentSymbol => ({
+        name: ast.variable.text,
+        kind: SymbolKind.Namespace,
+        range: document.getNodeRange(ast),
+        selectionRange: document.getNodeRange(ast.variable),
+    })
+
+    const handleEntityDeclaration = (ast: EntityDeclaration): DocumentSymbol => ({
+        name: renderBasicStructureDeclaration(ast.declaration),
+        kind: SymbolKind.Class,
+        range: document.getNodeRange(ast),
+        selectionRange: document.getNodeRange(ast.declaration.name),
+        children: ast.children.map(x => handleEntityMetaDeclaration(x)),
+    })
+
+    const handleEntityMetaDeclaration = (ast: EntityMetaDeclaration): DocumentSymbol => ({
+        name: renderBasicPropertyDeclaration(ast.declaration),
+        kind: SymbolKind.Variable,
+        range: document.getNodeRange(ast),
+        selectionRange: document.getNodeRange(ast.declaration.name.variable),
+        children: ast.children.map(x => handlePropertyDeclaration(x)),
+    })
+
+    const handlePropertyDeclaration = (ast: PropertyDeclaration): DocumentSymbol => ({
+        name: renderBasicStructureDeclaration(ast.declaration),
+        kind: SymbolKind.Property,
+        range: document.getNodeRange(ast),
+        selectionRange: document.getNodeRange(ast.declaration.name),
+    })
 
     return document.ast.nodes.map(node => {
         switch (node.type) {
