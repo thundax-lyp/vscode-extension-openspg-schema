2213f6318be1d371899e3f8b021c22d8c2901b62
thundax-lyp <731378491@qq.com>
2025-12-12 18:38:58 +0800

    init

---
+++ openspg-schema-language-server/server/src/core/common/generate.ts
@@ -0,0 +1,12 @@
+import * as syntax from "openspg-schema-antlr4";
+
+export const generate = async (ast: syntax.SyntaxNode, options: syntax.GenerateOptions = {}): Promise<string> => {
+    return syntax.generate(ast, {
+        ...options,
+        plugins: [{
+            languages: syntax.languages,
+            parsers: syntax.parsers,
+            printers: syntax.printers,
+        }],
+    })
+};
