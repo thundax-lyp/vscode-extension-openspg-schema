b0ce16de20157b61194c6ff1e36d40f34cd8613c
thundax-lyp <731378491@qq.com>
2025-12-30 14:11:13 +0800

    refactor

--- openspg-schema-language-server/server/src/core/folding/on-folding-ranges.ts
+++ openspg-schema-language-server/server/src/core/folding/on-folding-ranges.ts
@@ -1,46 +1,51 @@
-import {FoldingRange} from 'vscode-languageserver';
+import {FoldingRange, Range} from 'vscode-languageserver';
 import * as syntax from 'openspg-schema-antlr4'
 import {Context, OnFoldingRanges} from '../context';
 
-export const onFoldingRanges: OnFoldingRanges = ({connection, documents}: Context) => async ({textDocument}) => {
+export const onFoldingRanges: OnFoldingRanges = ({documents}: Context) => async ({textDocument}) => {
     const document = documents.get(textDocument.uri);
     if (!document || !document.ast) {
         return null;
     }
 
+    const trimRange = (range: [number, number]) => {
+        const plainContent = document.getText(Range.create(
+            document.positionAt(range[0]), document.positionAt(range[1])
+        ));
+        const start = range[0] + plainContent.length - plainContent.trimStart().length;
+        const end = range[1] - plainContent.length + plainContent.trimEnd().length;
+        return [start, end];
+    }
+
     const foldingRanges: FoldingRange[] = []
 
-    // syntax.visit(document.ast, {
-    //     BasicPropertyName: ({node}) => emit(node, SemanticTokenTypes.property),
-    //     BasicPropertyValue: ({node}) => emit(node, SemanticTokenTypes.string),
-    //
-    //     BasicStructureType: ({node}) => emit(node, SemanticTokenTypes.keyword),
-    //
-    //     BlockPropertyValue: ({node}) => emit(node, SemanticTokenTypes.string),
-    //
-    //     BuiltinPropertyName: ({node}) => emit(node, SemanticTokenTypes.keyword),
-    //     BuiltinPropertyValue: ({node}) => emit(node, SemanticTokenTypes.keyword),
-    //     KnowledgeStructureType: ({node}) => emit(node, SemanticTokenTypes.keyword),
-    //     StandardStructureType: ({node}) => emit(node, SemanticTokenTypes.keyword),
-    //
-    //     StructureAlias: ({node}) => emit(node, SemanticTokenTypes.string),
-    //
-    //     StructureRealName: ({node, path}) => {
-    //         if (path.split('.').includes('BasicStructureTypeExpression')) {
-    //             emit(node, SemanticTokenTypes.modifier);
-    //         } else {
-    //             emit(node, SemanticTokenTypes.struct);
-    //         }
-    //     },
-    //
-    //     StructureSemanticName: ({node, path}) => {
-    //         if (path.split('.').includes('BasicStructureTypeExpression')) {
-    //             emit(node, SemanticTokenTypes.modifier);
-    //         } else {
-    //             emit(node, SemanticTokenTypes.struct);
-    //         }
-    //     },
-    // })
+    syntax.visit(document.ast, {
+        EntityDeclaration: ({node}) => {
+            if (node.children && node.children.length > 0) {
+                const range = trimRange(node.range);
+                foldingRanges.push(FoldingRange.create(
+                    document.positionAt(range[0]).line, document.positionAt(range[1]).line
+                ));
+            }
+        },
+        PropertyDeclaration: ({node}) => {
+            if (node.children && node.children.length > 0) {
+                const range = trimRange(node.range);
+                foldingRanges.push(FoldingRange.create(
+                    document.positionAt(range[0]).line, document.positionAt(range[1]).line
+                ));
+            }
+        },
+        BlockPropertyValue: ({node}) => {
+            const start = document.positionAt(node.range[0])
+            const end = document.positionAt(node.range[1])
+            if (start.line < end.line) {
+                foldingRanges.push(FoldingRange.create(
+                    start.line, end.line
+                ));
+            }
+        }
+    })
 
     return foldingRanges;
 };
