908592b9e3067e995ff8d4a82500561fba836838
thundax-lyp <731378491@qq.com>
2026-01-04 12:29:13 +0800

    refactor

--- openspg-concept-rule-language-server/server/src/core/diagnostic/on-diagnostics.ts
+++ openspg-concept-rule-language-server/server/src/core/diagnostic/on-diagnostics.ts
@@ -14,6 +14,17 @@ export const onDiagnostics = (ctx: Context): OnDiagnostics => async ({textDocume
     const validator = new FullValidator();
 
     const items: Diagnostic[] = validator.validate(document!);
+    items.sort((o1, o2) => {
+        if (o1.range.start.line !== o2.range.start.line) {
+            return o1.range.start.line - o2.range.start.line
+        } else if (o1.range.start.character !== o2.range.start.character) {
+            return o1.range.start.character - o2.range.start.character;
+        } else if (o1.range.end.line !== o2.range.end.line) {
+            return o1.range.end.line - o2.range.end.line;
+        } else {
+            return o1.range.end.character - o2.range.end.character;
+        }
+    });
 
     return {
         kind: DocumentDiagnosticReportKind.Full, items
