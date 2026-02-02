b567b950e934f275432a5756db8bdbb79d2231c2
thundax-lyp <731378491@qq.com>
2026-02-02 10:28:18 +0800

    Test: generator

--- openspg-schema-antlr4/test/generator.test.ts
+++ openspg-schema-antlr4/test/generator.test.ts
@@ -1,5 +1,21 @@
-import {test} from 'vitest';
+import {expect, test} from 'vitest';
+import {generate, parse} from "../src";
 
-test("generator", () => {
+test("generator", async () => {
 
-})
+    const sourceCode = `
+Person(人物) ->  EntityType:
+    desc :  description
+    properties :
+        Good(好) : ConceptType
+`
+
+    const expectedCode = `Person(人物)-> EntityType:
+  desc: description
+  properties:
+    Good(好): ConceptType`
+
+    const ast = parse(sourceCode);
+
+    expect(await generate(ast.nodes[0])).toBe(expectedCode)
+});
