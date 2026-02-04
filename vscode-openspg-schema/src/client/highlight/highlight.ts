d4c5968b08cc3ff242a2d5090e34bd3c89453c3e
thundax-lyp <731378491@qq.com>
2026-02-04 15:12:34 +0800

    prettier

--- vscode-openspg-schema/src/client/highlight/highlight.ts
+++ vscode-openspg-schema/src/client/highlight/highlight.ts
@@ -1,10 +1,18 @@
-import {CancellationToken, DocumentHighlight, DocumentHighlightProvider, Position, ProviderResult, TextDocument} from "vscode";
-
+import {
+    CancellationToken,
+    DocumentHighlight,
+    DocumentHighlightProvider,
+    Position,
+    ProviderResult,
+    TextDocument
+} from 'vscode';
 
 export class SchemaDocumentHighlightProvider implements DocumentHighlightProvider {
-
-    provideDocumentHighlights(document: TextDocument, position: Position, token: CancellationToken): ProviderResult<DocumentHighlight[]> {
-        throw new Error("Method not implemented.");
+    provideDocumentHighlights(
+        document: TextDocument,
+        position: Position,
+        token: CancellationToken
+    ): ProviderResult<DocumentHighlight[]> {
+        throw new Error('Method not implemented.');
     }
-
 }
