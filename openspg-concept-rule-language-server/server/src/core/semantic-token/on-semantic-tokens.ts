8cfc58b360ef9220a6ec4bf01ba5ce16ec54c9a6
thundax-lyp <731378491@qq.com>
2026-02-05 14:14:22 +0800

    prettier

--- openspg-concept-rule-language-server/server/src/core/semantic-token/on-semantic-tokens.ts
+++ openspg-concept-rule-language-server/server/src/core/semantic-token/on-semantic-tokens.ts
@@ -1,77 +1,82 @@
-import {SemanticTokenModifiers, SemanticTokensBuilder, SemanticTokenTypes} from 'vscode-languageserver';
-import * as syntax from 'openspg-concept-rule-antlr4'
-import {Context, OnSemanticTokens} from '../context';
+import { SemanticTokenModifiers, SemanticTokensBuilder, SemanticTokenTypes } from 'vscode-languageserver';
+import * as syntax from 'openspg-concept-rule-antlr4';
+import { Context, OnSemanticTokens } from '../context';
 
+export const onSemanticTokens =
+    (ctx: Context): OnSemanticTokens =>
+    async ({ textDocument }) => {
+        const builder = new SemanticTokensBuilder();
 
-export const onSemanticTokens = (ctx: Context): OnSemanticTokens => async ({textDocument}) => {
-    const builder = new SemanticTokensBuilder();
+        const document = ctx.documents.get(textDocument.uri);
+        if (!document || !(await document.isReady())) {
+            return builder.build();
+        }
 
-    const document = ctx.documents.get(textDocument.uri);
-    if (!document || !document.ast) {
+        const tokenTypes = Object.values(SemanticTokenTypes);
+        const tokenModifiers = Object.values(SemanticTokenModifiers);
+
+        const emit = (
+            node: syntax.SyntaxNode,
+            tokenType: SemanticTokenTypes,
+            tokenModifier: SemanticTokenModifiers = SemanticTokenModifiers.declaration
+        ) => {
+            const { line, column } = node.location.start;
+            builder.push(
+                line - 1,
+                column,
+                node.range[1] - node.range[0] + 1,
+                tokenTypes.indexOf(tokenType),
+                tokenModifiers.indexOf(tokenModifier)
+            );
+        };
+
+        const emitPrefixKeyword = (
+            node: syntax.SyntaxNode,
+            prefix: string,
+            tokenType: SemanticTokenTypes = SemanticTokenTypes.keyword,
+            tokenModifier: SemanticTokenModifiers = SemanticTokenModifiers.declaration
+        ) => {
+            const { line, column } = node.location.start;
+            builder.push(
+                line - 1,
+                column,
+                prefix.length,
+                tokenTypes.indexOf(tokenType),
+                tokenModifiers.indexOf(tokenModifier)
+            );
+        };
+
+        syntax.visit(document.ast!, {
+            NamespaceDeclaration: ({ node }) => emitPrefixKeyword(node, 'namespace'),
+            NamespaceVariable: ({ node }) => emit(node, SemanticTokenTypes.variable),
+
+            RuleWrapperRuleHead: ({ node }) => emitPrefixKeyword(node, 'rule'),
+            ConceptRuleHead: ({ node }) => emitPrefixKeyword(node, 'define'),
+
+            TheGraphStructureHead: ({ node }) => emit(node, SemanticTokenTypes.keyword),
+            TheRuleHead: ({ node }) => emit(node, SemanticTokenTypes.keyword),
+            TheActionHead: ({ node }) => emit(node, SemanticTokenTypes.keyword),
+
+            ElementVariableDeclaration: ({ node }) => emit(node, SemanticTokenTypes.variable),
+
+            ConceptInstanceId: ({ node, parentPath }) => {
+                const parents = parentPath?.path.split('.') ?? [];
+                if (parents.includes('RuleWrapperHead')) {
+                    emit(node, SemanticTokenTypes.class);
+                } else if (parents.includes('ConceptName')) {
+                    emit(node, SemanticTokenTypes.variable);
+                }
+            },
+
+            Identifier: ({ node, parentPath }) => {
+                const parents = parentPath?.path.split('.') ?? [];
+                if (parents.includes('RuleWrapperHead')) {
+                    emit(node, SemanticTokenTypes.class);
+                } else if (parents.includes('ConceptType')) {
+                    emit(node, SemanticTokenTypes.variable);
+                }
+            }
+        });
+
         return builder.build();
-    }
-
-    const tokenTypes = Object.values(SemanticTokenTypes)
-    const tokenModifiers = Object.values(SemanticTokenModifiers)
-
-    const emit = (
-        node: syntax.SyntaxNode,
-        tokenType: SemanticTokenTypes,
-        tokenModifier: SemanticTokenModifiers = SemanticTokenModifiers.declaration
-    ) => {
-        const {line, column} = node.location.start;
-        builder.push(
-            line - 1, column, node.range[1] - node.range[0] + 1,
-            tokenTypes.indexOf(tokenType),
-            tokenModifiers.indexOf(tokenModifier)
-        )
-    }
-
-    const emitPrefixKeyword = (
-        node: syntax.SyntaxNode,
-        prefix: string,
-        tokenType: SemanticTokenTypes = SemanticTokenTypes.keyword,
-        tokenModifier: SemanticTokenModifiers = SemanticTokenModifiers.declaration
-    ) => {
-        const {line, column} = node.location.start;
-        builder.push(
-            line - 1, column, prefix.length,
-            tokenTypes.indexOf(tokenType),
-            tokenModifiers.indexOf(tokenModifier)
-        )
-    }
-
-    syntax.visit(document.ast, {
-        NamespaceDeclaration: ({node}) => emitPrefixKeyword(node, 'namespace'),
-        NamespaceVariable: ({node}) => emit(node, SemanticTokenTypes.variable),
-
-        RuleWrapperRuleHead: ({node}) => emitPrefixKeyword(node, 'rule'),
-        ConceptRuleHead: ({node}) => emitPrefixKeyword(node, 'define'),
-
-        TheGraphStructureHead: ({node}) => emit(node, SemanticTokenTypes.keyword),
-        TheRuleHead: ({node}) => emit(node, SemanticTokenTypes.keyword),
-        TheActionHead: ({node}) => emit(node, SemanticTokenTypes.keyword),
-
-        ElementVariableDeclaration: ({node}) => emit(node, SemanticTokenTypes.variable),
-
-        ConceptInstanceId: ({node, parentPath}) => {
-            const parents = parentPath?.path.split('.') ?? []
-            if (parents.includes('RuleWrapperHead')) {
-                emit(node, SemanticTokenTypes.class)
-            } else if (parents.includes('ConceptName')) {
-                emit(node, SemanticTokenTypes.variable)
-            }
-        },
-
-        Identifier: ({node, parentPath}) => {
-            const parents = parentPath?.path.split('.') ?? []
-            if (parents.includes('RuleWrapperHead')) {
-                emit(node, SemanticTokenTypes.class)
-            } else if (parents.includes('ConceptType')) {
-                emit(node, SemanticTokenTypes.variable)
-            }
-        },
-    })
-
-    return builder.build();
-}
+    };
--- openspg-concept-rule-language-server/server/src/core/symbol/on-document-symbol.ts
+++ openspg-concept-rule-language-server/server/src/core/symbol/on-document-symbol.ts
@@ -1,97 +1,103 @@
-import {DocumentSymbol, SymbolKind} from 'vscode-languageserver';
-import * as syntax from "openspg-concept-rule-antlr4";
-import {OnDocumentSymbol} from '../context';
-import {generate} from "../common";
+import { DocumentSymbol, SymbolKind } from 'vscode-languageserver';
+import * as syntax from 'openspg-concept-rule-antlr4';
+import { OnDocumentSymbol } from '../context';
+import { generate } from '../common';
 
-export const onDocumentSymbol: OnDocumentSymbol = (ctx) => async ({textDocument}) => {
-    const document = ctx.documents.get(textDocument.uri);
-    if (!document || !document.ast) {
-        return null;
-    }
-
-    const handleNamespaceDeclaration = async (ast: syntax.NamespaceDeclaration): Promise<DocumentSymbol> => ({
-        name: await generate(ast.variable),
-        kind: SymbolKind.Namespace,
-        range: document.getNodeRange(ast),
-        selectionRange: document.getNodeRange(ast.variable),
-    })
-
-    const handleRuleWrapperDeclaration = async (ast: syntax.RuleWrapperDeclaration): Promise<DocumentSymbol> => {
-        let name = await generate(ast.head)
-        if (name.endsWith(':')) {
-            name = name.substring(0, name.length - 1);
+export const onDocumentSymbol: OnDocumentSymbol =
+    (ctx) =>
+    async ({ textDocument }) => {
+        const document = ctx.documents.get(textDocument.uri);
+        if (!document || !(await document.isReady())) {
+            return null;
         }
-        return {
-            name,
-            kind: SymbolKind.Class,
+
+        const handleNamespaceDeclaration = async (ast: syntax.NamespaceDeclaration): Promise<DocumentSymbol> => ({
+            name: await generate(ast.variable),
+            kind: SymbolKind.Namespace,
             range: document.getNodeRange(ast),
-            selectionRange: document.getNodeRange(ast.head),
-            children: await Promise.all(
-                ast.rules
-                    .flatMap(x => x.conceptRules)
-                    .map(x => handleConceptRuleDeclaration(x))
-            )
-        }
-    }
+            selectionRange: document.getNodeRange(ast.variable)
+        });
 
-    const handleConceptRuleDeclaration = async (ast: syntax.ConceptRuleDeclaration): Promise<DocumentSymbol> => {
-        let name = await generate(ast.head);
-        if (name.startsWith('Define')) {
-            name = name.substring(6).trim();
-        }
-        return {
-            name,
-            kind: SymbolKind.Struct,
-            range: document.getNodeRange(ast),
-            selectionRange: document.getNodeRange(ast.head),
-            children: await Promise.all(
-                ast.children.flatMap(x => {
-                    if (x.type === 'TheGraphStructureDeclaration') {
-                        return [handleTheGraphStructureDeclaration(x)]
-                    } else if (x.type === 'TheRuleDeclaration') {
-                        return [handleTheRuleDeclaration(x)]
-                    } else if (x.type === 'TheActionDeclaration') {
-                        return [handleTheActionDeclaration(x)]
-                    }
-                    return []
-                })
-            )
-        }
-    }
-
-    const handleTheGraphStructureDeclaration = async (ast: syntax.TheGraphStructureDeclaration): Promise<DocumentSymbol> => ({
-        name: await generate(ast.head),
-        kind: SymbolKind.Function,
-        range: document.getNodeRange(ast),
-        selectionRange: document.getNodeRange(ast.head),
-    })
-
-    const handleTheRuleDeclaration = async (ast: syntax.TheRuleDeclaration): Promise<DocumentSymbol> => ({
-        name: await generate(ast.head),
-        kind: SymbolKind.Function,
-        range: document.getNodeRange(ast),
-        selectionRange: document.getNodeRange(ast.head),
-    })
-
-    const handleTheActionDeclaration = async (ast: syntax.TheActionDeclaration): Promise<DocumentSymbol> => ({
-        name: await generate(ast.head),
-        kind: SymbolKind.Function,
-        range: document.getNodeRange(ast),
-        selectionRange: document.getNodeRange(ast.head),
-    })
-
-    return (await Promise.all(
-        document.ast.nodes.map(node => (async () => {
-            switch (node.type) {
-                case 'NamespaceDeclaration':
-                    return await handleNamespaceDeclaration(node);
-                case 'RuleWrapperDeclaration':
-                    return await handleRuleWrapperDeclaration(node);
-                case 'ConceptRuleDeclaration':
-                    return await handleConceptRuleDeclaration(node);
-                default:
-                    return null;
+        const handleRuleWrapperDeclaration = async (ast: syntax.RuleWrapperDeclaration): Promise<DocumentSymbol> => {
+            let name = await generate(ast.head);
+            if (name.endsWith(':')) {
+                name = name.substring(0, name.length - 1);
             }
-        })())
-    )).filter(Boolean) as DocumentSymbol[];
-}
+            return {
+                name,
+                kind: SymbolKind.Class,
+                range: document.getNodeRange(ast),
+                selectionRange: document.getNodeRange(ast.head),
+                children: await Promise.all(
+                    ast.rules.flatMap((x) => x.conceptRules).map((x) => handleConceptRuleDeclaration(x))
+                )
+            };
+        };
+
+        const handleConceptRuleDeclaration = async (ast: syntax.ConceptRuleDeclaration): Promise<DocumentSymbol> => {
+            let name = await generate(ast.head);
+            if (name.startsWith('Define')) {
+                name = name.substring(6).trim();
+            }
+            return {
+                name,
+                kind: SymbolKind.Struct,
+                range: document.getNodeRange(ast),
+                selectionRange: document.getNodeRange(ast.head),
+                children: await Promise.all(
+                    ast.children.flatMap((x) => {
+                        if (x.type === 'TheGraphStructureDeclaration') {
+                            return [handleTheGraphStructureDeclaration(x)];
+                        } else if (x.type === 'TheRuleDeclaration') {
+                            return [handleTheRuleDeclaration(x)];
+                        } else if (x.type === 'TheActionDeclaration') {
+                            return [handleTheActionDeclaration(x)];
+                        }
+                        return [];
+                    })
+                )
+            };
+        };
+
+        const handleTheGraphStructureDeclaration = async (
+            ast: syntax.TheGraphStructureDeclaration
+        ): Promise<DocumentSymbol> => ({
+            name: await generate(ast.head),
+            kind: SymbolKind.Function,
+            range: document.getNodeRange(ast),
+            selectionRange: document.getNodeRange(ast.head)
+        });
+
+        const handleTheRuleDeclaration = async (ast: syntax.TheRuleDeclaration): Promise<DocumentSymbol> => ({
+            name: await generate(ast.head),
+            kind: SymbolKind.Function,
+            range: document.getNodeRange(ast),
+            selectionRange: document.getNodeRange(ast.head)
+        });
+
+        const handleTheActionDeclaration = async (ast: syntax.TheActionDeclaration): Promise<DocumentSymbol> => ({
+            name: await generate(ast.head),
+            kind: SymbolKind.Function,
+            range: document.getNodeRange(ast),
+            selectionRange: document.getNodeRange(ast.head)
+        });
+
+        return (
+            await Promise.all(
+                document.ast!.nodes.map((node) =>
+                    (async () => {
+                        switch (node.type) {
+                            case 'NamespaceDeclaration':
+                                return await handleNamespaceDeclaration(node);
+                            case 'RuleWrapperDeclaration':
+                                return await handleRuleWrapperDeclaration(node);
+                            case 'ConceptRuleDeclaration':
+                                return await handleConceptRuleDeclaration(node);
+                            default:
+                                return null;
+                        }
+                    })()
+                )
+            )
+        ).filter(Boolean) as DocumentSymbol[];
+    };
