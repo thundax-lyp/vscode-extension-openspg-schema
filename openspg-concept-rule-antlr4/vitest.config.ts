9d24f8838cfa76eba3fe184959212193cf9e5dd5
thundax-lyp <731378491@qq.com>
2026-02-06 14:49:50 +0800

    lint & prettier

--- openspg-concept-rule-antlr4/vitest.config.ts
+++ openspg-concept-rule-antlr4/vitest.config.ts
@@ -1,10 +1,10 @@
-import {defineConfig} from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
         coverage: {
-            include: ['test/**/*.ts'],
-            exclude: ['.idea', '.git', 'node_modules', 'dist', 'docs', 'grammar', 'scripts', 'src/antlr4'],
-        },
-    },
+            include: ["test/**/*.ts"],
+            exclude: [".idea", ".git", "node_modules", "dist", "docs", "grammar", "scripts", "src/antlr4"]
+        }
+    }
 });
